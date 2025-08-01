469c97376723a2e6dbcdfa3b57df50c6eb0ee647
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-08-01 15:28:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,6 @@ app.use(express.static("./public"));
   app.use("/api/proemovalsetting", authAuroRemovalProperty);
 
   app.use("/api/listing", authListing);
-  app.listen(8010, () => {
+  app.listen(8016, () => {
     console.log("App is running ");
   });
